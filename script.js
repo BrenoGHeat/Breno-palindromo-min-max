6e76f0c2e068c1a37c1d919169edeb6c98029b4d
Breno Garcia <BrenoGHeat@gmail.com>
2023-12-12 16:28:13 -0300

    tarefa 1 concluída

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-function isPalindrome(str){
-    /* Seu código aqui */
+function isPalindrome(palavra){
+   
+    const frase = palavra.toLowerCase().replaceAll(" ","");
+    let palavraInvertida = "" ; 
+
+    for(let i = frase.length - 1 ; i >= 0 ; i--){
+        palavraInvertida = palavraInvertida + frase[i];
+
+    }
+
+        if(frase == palavraInvertida){
+            return true
+        }
+            return false
 }
+
+
+
+
+
 
 function arrayMaxMin(arr){
     /* Seu código aqui */
