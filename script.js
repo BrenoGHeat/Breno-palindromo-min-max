a879da6d4a3506863ff415ac471f2c358e44e7a8
Breno Garcia <BrenoGHeat@gmail.com>
2023-12-13 14:01:12 -0300

    function arrayMaxMin resolvido

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function isPalindrome(palavra){
 
     for(let i = frase.length - 1 ; i >= 0 ; i--){
         palavraInvertida = palavraInvertida + frase[i];
+        console.log(palavraInvertida);
 
     }
 
@@ -15,10 +16,22 @@ function isPalindrome(palavra){
 }
 
 
-
-
-
-
 function arrayMaxMin(arr){
-    /* Seu código aqui */
+  
+   let valorMaximo = arr[0];
+   let valorMinimo = arr[0];
+
+   for(let i = 0 ; i < arr.length ; i++){
+       if(arr[i] < valorMinimo){  
+        valorMinimo =  arr[i];
+       } 
+       if( arr[i] > valorMaximo){
+        valorMaximo = arr[i];
+       }
+    }
+        console.log(valorMaximo, valorMinimo);
+        console.log(arr);
+
+   return [valorMinimo , valorMaximo];
 }
+
